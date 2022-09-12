4abefee25a8a5d428a9dd137c15c8ff160c04a9b
Tommymarvel <ibironketomiwa4@gmail.com>
2022-09-12 16:58:35 +0100

    fix linter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// eslint-disable-next-line react/prefer-stateless-function 
 class App extends Component {
   render() {
     return (
