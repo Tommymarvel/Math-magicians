2f39b4db5a8a43286064d92aa2b2f4edcce9f942
Tommymarvel <ibironketomiwa4@gmail.com>
2023-03-02 15:36:01 +0100

    solve eslint error

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,14 +5,22 @@ const Homepage = () => (
   <div className="homepageContent">
     <h2>Welcome to our page!</h2>
     <p className="lorem">
-      Welcome to the Math Magician website, where we celebrate the wonders of mathematics!
-  As Albert Einstein once said, 
-  "Pure mathematics is, in its way, the poetry of logical ideas." 
-  Whether you are a math enthusiast, a student, or just someone who appreciates 
-  the beauty of numbers and equations, this website is the perfect place for you.
+      Welcome to the Math Magician website, where we celebrate the wonders of
+      mathematics! As Albert Einstein once said, &quot;Pure mathematics is, in
+      its way, the poetry of logical ideas.&quot; Whether you are a math
+      enthusiast, a student, or just someone who appreciates the beauty of
+      numbers and equations, this website is the perfect place for you.
     </p>
     <p className="lorem">
-      Our Single Page App (SPA) allows you to make simple calculations and explore the power of math in a fun and interactive way. You can test your skills with basic arithmetic operations, or challenge yourself with more complex calculations. Additionally, you can read random math quotes from some of the greatest minds in history, gaining insights and inspiration from the likes of Isaac Newton, Ada Lovelace, and Euclid. So, whether you are looking to sharpen your math skills or simply want to learn more about the fascinating world of mathematics, the Math Magician website has something for everyone.
+      Our Single Page App (SPA) allows you to make simple calculations and
+      explore the power of math in a fun and interactive way. You can test your
+      skills with basic arithmetic operations, or challenge yourself with more
+      complex calculations. Additionally, you can read random math quotes from
+      some of the greatest minds in history, gaining insights and inspiration
+      from the likes of Isaac Newton, Ada Lovelace, and Euclid. So, whether you
+      are looking to sharpen your math skills or simply want to learn more about
+      the fascinating world of mathematics, the Math Magician website has
+      something for everyone.
     </p>
   </div>
 );
